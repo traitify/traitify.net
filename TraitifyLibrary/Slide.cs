7ba92440c71da190589ac14c1e59c868983bc563
David Fekke <dfekke@yourmsc.com>
2014-05-15 10:35:04 -0400

    Added rest of the methods

--- TraitifyLibrary/Slide.cs
+++ TraitifyLibrary/Slide.cs
@@ -8,15 +8,15 @@ namespace com.traitify.net.TraitifyLibrary
     public class Slide
     {
         public String id { get; set; }
-        public int position { get; set; }
+        public Nullable<int> position { get; set; }
         public String caption { get; set; }
         public String image_desktop { get; set; }
         public String image_desktop_retina { get; set; }
         public String image_phone_landscape { get; set; }
         public String image_phone_portrait { get; set; }
         public Boolean response { get; set; }
-        public int time_taken { get; set; }
-        public DateTime completed_at { get; set; }
-        public DateTime created_at { get; set; }
+        public Nullable<int> time_taken { get; set; }
+        public Nullable<DateTime> completed_at { get; set; }
+        public Nullable<DateTime> created_at { get; set; }
     }
 }
--- TraitifyLibrary/Traitify.cs
+++ TraitifyLibrary/Traitify.cs
@@ -18,6 +18,13 @@ namespace com.traitify.net.TraitifyLibrary
         private string _publicKey = String.Empty;
         private string _version = String.Empty;
 
+        /// <summary>
+        /// Traitify Constructor
+        /// </summary>
+        /// <param name="host">Host URI</param>
+        /// <param name="publicKey">Public Key string</param>
+        /// <param name="secretKey">Private / Secret Key string</param>
+        /// <param name="version">API version number</param>
         public Traitify(string host, string publicKey, string secretKey, string version)
         {
             _host = host;
@@ -26,7 +33,11 @@ namespace com.traitify.net.TraitifyLibrary
             _version = version;
         }
 
-        //createAssessment: function(deckId, callBack){
+        /// <summary>
+        /// CreateAssesment method
+        /// </summary>
+        /// <param name="deckId"></param>
+        /// <returns>Assessment</returns>
         public Assessment CreateAssesment(string deckId)
         {
             Assessment assessment = default(Assessment);
@@ -35,14 +46,17 @@ namespace com.traitify.net.TraitifyLibrary
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 var jsonDeck =  JsonConvert.SerializeObject(new { deck_id = deckId }); 
                 var response = client.PostAsync(_version + "/assessments", new StringContent(jsonDeck, Encoding.UTF8, "application/json")).Result;
-
                 assessment = JsonConvert.DeserializeObject<Assessment>(response.Content.ReadAsStringAsync().Result);
             }
 
             return assessment;
         }
 
-        //getAssessment: function(assessmentId, callBack){
+        /// <summary>
+        /// GetAssessment
+        /// </summary>
+        /// <param name="assessmentId"></param>
+        /// <returns>Assessment</returns>
         public Assessment GetAssessment(string assessmentId)
         {
             Assessment assessment = default(Assessment);
@@ -50,28 +64,57 @@ namespace com.traitify.net.TraitifyLibrary
             {
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 var response = client.GetAsync(_version + "/assessments/" + assessmentId).Result;
-                string result = response.Content.ReadAsStringAsync().Result;
                 assessment = JsonConvert.DeserializeObject<Assessment>(response.Content.ReadAsStringAsync().Result);
             }
             return assessment;
         }
 
-        //getSlides: function(assessmentId, callBack){
+        /// <summary>
+        /// GetSlides
+        /// </summary>
+        /// <param name="assessmentId"></param>
+        /// <returns>List<Slide></returns>
         public List<Slide> GetSlides(string assessmentId)
         {
-            return new List<Slide>();
+            List<Slide> slides = default(List<Slide>);
+            using (var client = new HttpClient(new AuthHandler(_secretKey)) { BaseAddress = new Uri(_host) })
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = client.GetAsync(_version + "/assessments/" + assessmentId + "/slides").Result;
+                slides = JsonConvert.DeserializeObject<List<Slide>>(response.Content.ReadAsStringAsync().Result);
+            }
+            return slides;
         }
 
-        //setSlide: function(assessmentId, slideId, params, callBack){
+        /// <summary>
+        /// SetSlide
+        /// </summary>
+        /// <param name="assessmentId"></param>
+        /// <param name="slideId"></param>
         public void SetSlide(string assessmentId, string slideId)
         {
-
+            using (var client = new HttpClient(new AuthHandler(_secretKey)) { BaseAddress = new Uri(_host) })
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.PutAsync(_version + "/assessments/" + assessmentId + "/slides/" + slideId, null);
+            }
         }
 
-        //getPersonalityTypes: function(assessmentId, callBack){
+        /// <summary>
+        /// GetPersonalityTypes
+        /// </summary>
+        /// <param name="assessmentId"></param>
+        /// <returns>List<PersonalityType></returns>
         public List<PersonalityType> GetPersonalityTypes(string assessmentId)
         {
-            return new List<PersonalityType>();
+            List<PersonalityType> personalityTypes = default(List<PersonalityType>);
+            using (var client = new HttpClient(new AuthHandler(_secretKey)) { BaseAddress = new Uri(_host) })
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = client.GetAsync(_version + "/assessments/" + assessmentId + "/personality_types").Result;
+                personalityTypes = JsonConvert.DeserializeObject<List<PersonalityType>>(response.Content.ReadAsStringAsync().Result);
+            }
+            return personalityTypes;
         }
 
     }
