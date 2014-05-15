cad7c5533dbd94d1e7aaf380461c45e0205a43ef
David Fekke <david@fekke.com>
2014-05-15 09:09:39 -0400

    Update UnitTest
    Commented out parameters.

--- TraitifyUnitTests/TraitifyUnitTests.cs
+++ TraitifyUnitTests/TraitifyUnitTests.cs
@@ -13,16 +13,16 @@ namespace TraitifyUnitTests
             //Base URL: https://api-staging.traitify.com. 
             //Private/Secret Key: qrn5u1ocl0niru9ntl0160njrc
             //Public Key: qop22g5tmrcgl270nuhel9cq9g
-            Traitify trait = new Traitify("https://api-staging.traitify.com", "qop22g5tmrcgl270nuhel9cq9g", "qrn5u1ocl0niru9ntl0160njrc", "v1");
-            var response = trait.CreateAssesment("304d0392-4a08-4ef6-a996-224324a9f6f8");
+            Traitify trait = new Traitify("https://???", "???", "???", "v1");
+            var response = trait.CreateAssesment("??");
         }
 
         //Get Assessment Slides
         [TestMethod]
         public void TestGetAssessment()
         {
-            Traitify trait = new Traitify("https://api-staging.traitify.com", "qop22g5tmrcgl270nuhel9cq9g", "qrn5u1ocl0niru9ntl0160njrc", "v1");
-            var response = trait.GetAssessment("35c80356-9a5c-4ef7-97d9-d46d5d1726d8");
+            Traitify trait = new Traitify("???", "???", "???", "v1");
+            var response = trait.GetAssessment("????");
         }
 
         //Get Assessment Slides
