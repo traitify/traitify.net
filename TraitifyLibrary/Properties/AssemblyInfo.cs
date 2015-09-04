566e4d19df0875c76eb59c2ae26e02dbce28a58b
raduflorescu <radu.florescu@totaljobsgroup.com>
2015-09-04 17:19:56 +0100

    Added nuspec file and changed description

--- TraitifyLibrary/Properties/AssemblyInfo.cs
+++ TraitifyLibrary/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TraitifyLibrary")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Traitify is a .NET 4 wrapper for the Traitify Developer's API.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("floradu88")]
 [assembly: AssemblyProduct("TraitifyLibrary")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
